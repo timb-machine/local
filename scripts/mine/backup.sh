1cd24b41901948d000a98c7cdd58322662540d1b
timb <timb@da283472-e30e-4bd4-a68d-706198139375>
2015-03-03 20:14:14 +0000

    Added backup.sh
    git-svn-id: https://projects.nth-dimension.org.uk/subversion/local/trunk@187 da283472-e30e-4bd4-a68d-706198139375

---
+++ scripts/mine/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -s -p "Password: " password
+hostname="10.10.10.2"
+username="timb"
+sharename="timb"
+localdirectoryname="/mnt"
+remotedirectoryname="/backup"
+backupdirectorynames="/etc /usr/local /home /virtual"
+mount -o "username=${username},password=${password}" "//${hostname}/${sharename}" "${localdirectoryname}"
+if [ -d "${localdirectoryname}${remotedirectoryname}" ]
+then
+	for backupdirectoryname in ${backupdirectorynames}
+	do
+		rsync -avz --delete "${backupdirectoryname}" "${localdirectoryname}${remotedirectoryname}/"
+	done
+	umount "${localdirectoryname}"
+fi
